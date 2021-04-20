0d2a88def4ed0d910803e25f95f5f2018f87f836
Ezequiel Cruz <cruz.ezequiel.p@gmail.com>
2021-04-20 00:55:24 -0300

    Se agrego ItemCount como componente de carrito

---
+++ src/components/ItemCount.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import './NavBar.css';
+
+
+
+export default function ItemCount ({stock,inicial,item}){ 
+
+    let [count, setCount] = useState(inicial);
+    
+    function onAdd() {
+        if (count >= stock)
+        return; 
+        setCount(count + 1) 
+       }
+       
+       function offAdd() {
+        if (count <= inicial)
+        return; 
+        setCount(count - 1) 
+       }
+
+    return(
+        <div>
+
+            <div className="item__titulo">{item}</div>
+            <div className="item__estructura">
+            
+                <div>
+                    <button onClick= {offAdd} className="restar">-</button>
+                </div>
+                <div>
+                    {count}</div>
+                <div>
+                    <button onClick= {onAdd} className="sumar" >+</button>
+                </div>
+            </div>
+
+            <div className="item__titulo--añadirCarrito"> Agregar a carrito </div>
+
+        </div>
+    )
+   }
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import ItemCount from './ItemCount';
 
 function ItemListContainer(props){
     return(
         <div>
             <h1>Bienvenido a electro futuro</h1>
             {console.log(props)}
+            <ItemCount stock={5} inicial={1} item="Camisa Tiger" />
+            <ItemCount stock={10} inicial={3} item="Camisa Code" />  
         </div>  
     )
 }
