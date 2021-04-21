bad31350445d7836ed443dcd04fdf56575c23a61
Ezequiel Cruz <cruz.ezequiel.p@gmail.com>
2021-04-21 17:32:44 -0300

    Se agrego un delay en la muestra de productos

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,20 +2,18 @@ import React from 'react'
 
 
 
-
 export default function Item({post}) {
 
 
   return (
 
-
     <div>
-   
             <div>
-                Titulo: {post.title} 
-            
-            </div>
-                              
+              <div>Producto: {post.producto}</div>
+              <div>Categoría: {post.categoria}</div>
+              <div>Cantidad: {post.cantidad} </div>
+              <div>Precio: {post.precio}</div>   
+            </div>                        
     </div>
     
   )
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -8,25 +8,20 @@ function ItemListContainer(props){
 
     useEffect(() => {
         setTimeout(() => {
-            fetch('https://jsonplaceholder.typicode.com/todos')
+            fetch('https://raw.githubusercontent.com/ezequiel-cruz96/E-commerce--Electrofuturo--React-JS/master/src/listaProductos/productos.json')
                 .then((response) => response.json())
                 .then((data) => setPosts(data));
         }, 2000);
         
     }, [])
 
-   
-    
     return(
         <div>
             <h1>Bienvenido a electro futuro</h1>
             {console.log(props)}
             <ItemCount stock={5} inicial={1} item="Camisa Tiger" />
             <ItemCount stock={10} inicial={3} item="Camisa Code" /> 
-
             <List postsInput={posts}/> 
-
-
         </div>  
     )
 }
